205b6382ae44091e65cf5616eec51d8e0aa8f991
kanishksoni <42889372+kanishksoni@users.noreply.github.com>
2020-11-13 21:32:37 +0530

    Logout updated

--- client/src/components/SignOut.jsx
+++ client/src/components/SignOut.jsx
@@ -73,7 +73,7 @@ class SignOut extends React.Component {
                 <button
                   onClick={() => {
                     signout(() => {
-                      toast.error('Signout Successfully');
+                      toast.error('Logged Out Successfully');
                       window.location.reload();
                     });
                   }}
@@ -81,7 +81,7 @@ class SignOut extends React.Component {
                   className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
                 >
                   <i className='fas fa-user-plus fa 1x w-6  -ml-2' />
-                  <span className='ml-3'> Sign Out </span>
+                  <span className='ml-3'> Logout </span>
                 </button>
               </div>
               
